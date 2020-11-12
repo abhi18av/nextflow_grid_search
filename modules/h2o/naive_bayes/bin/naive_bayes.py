350fe6d6905fa3f19ec546ae715deb2e39f4f72f
Abhinav Sharma <abhi18av@gmail.com>
2020-11-12 12:04:35 +0530

    add the naive_bayes nextflow wrapper

--- modules/h2o/naive_bayes/bin/naive_bayes.py
+++ modules/h2o/naive_bayes/bin/naive_bayes.py
@@ -1,21 +1,3 @@
-nextflow.enable.dsl = 2
-
-params.train_frame = "https://s3.amazonaws.com/erin-data/higgs/higgs_train_10k.csv"
-params.test_frame = "https://s3.amazonaws.com/erin-data/higgs/higgs_test_5k.csv"
-
-process H2O_NAIVE_BAYES {
-    container "quay.io/abhi18av/nextflow_grid_search"
-
-    input:
-    tuple file(params.train_frame), file(params.test_frame)
-
-    output:
-    stdout auc
-
-    script:
-    """
-#!/usr/bin/env python3
-
 import h2o
 from h2o.estimators import H2ONaiveBayesEstimator
 h2o.init()
@@ -57,14 +39,4 @@ nb_model.train(x=predictors,
 test_perf = nb_model.model_performance(test)
 
 print(test_perf.auc())
-    """
-}
 
-//================================================================================
-// Module test
-//================================================================================
-
-workflow test {
-
-
-}
