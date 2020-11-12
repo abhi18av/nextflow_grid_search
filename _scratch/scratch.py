afd730a0c63ffedab5d341eaac911bd66d7e0011
Abhinav Sharma <abhi18av@gmail.com>
2020-11-12 12:21:58 +0530

    remove _scratch from being tracked

--- _scratch/scratch.py
+++
@@ -1,8 +0,0 @@
-import argparse
-parser = argparse.ArgumentParser()
-parser.add_argument("--train_frame", help='Path to the training data', required=True)
-parser.add_argument("--test_frame", help='Path to the testing data', required=True)
-args = parser.parse_args()
-
-print(args.train_frame)
-print(args.test_frame)
